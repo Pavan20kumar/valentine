462cbe07997173a9bf5a5c05c24aed0330fe3e9c
Pavan20kumar <154593775+Pavan20kumar@users.noreply.github.com>
2026-02-08 17:45:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ValentineApp from './mycomponet/valentine';
+import MessagePage from './mycomponet/valentine/MessagePage';
 
 function App() {
   return (
-    <div className="App">
-     <ValentineApp />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<ValentineApp />} />
+          <Route path="/message" element={<MessagePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
